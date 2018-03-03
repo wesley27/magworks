38747abe8ee88dc74b9cd318cd9d0db74df54a26
W. Wesley Weidenhamer II <www.wesley2@gmail.com>
2018-03-03 16:50:48 -0500

    clean up code, exception handling

--- magworks.py
+++ magworks.py
@@ -3,7 +3,6 @@ from reader import *
 def main():
     msr = Reader()
     msr.claim_reader()
-    print('\n')
     msr.read_ISO()
 
 main()
--- reader.py
+++ reader.py
@@ -2,7 +2,6 @@ import sys
 import usb.core
 import usb.util
 import codecs
-from collections import OrderedDict
 
 from codes import *
 
@@ -25,11 +24,17 @@ class Reader:
         
         msg = '\xc2%s' % TEST_COMM
         assert self.dev.ctrl_transfer(0x21, 9, 0x0300, 0, msg) == len(msg)
-        ret = self.dev.read(0x81, 1024, 5000)
+
+        try:
+            ret = self.dev.read(0x81, 1024, 5000)
+        except usb.core.USBError as e:
+            self.reset()
+            sys.exit('MagWorks lost connection to the card reader.')
+
         result = [hex(x) for x in ret]
 
         if result[1] == '0x1b' and result[2] == '0x79':
-            print('\t\t...connection is up and running.')
+            print('\t\t...connection is up and running.\n')
         else:
             sys.exit('MagWorks lost connection to the card reader.')
 
@@ -38,51 +43,47 @@ class Reader:
         print('Testing card reading sensor...')
         msg = '\xc2%s' % TEST_SENSOR
         assert self.dev.ctrl_transfer(0x21, 9, 0x300, 0, msg) == len(msg)
-        ret = self.dev.read(0x81, 1024, 5000)
+        print("Swipe card to test sensor.")
+
+        try:
+            ret = self.dev.read(0x81, 1024, 5000)
+        except usb.core.USBError as e:
+            self.reset()
+            print('\t\t...sensor test timed out. (WARNING, reader may still work\n')
+            return
 
         result = [hex(x) for x in ret]
         if result[1] == '0x1b' and result[2] == '0x30':
-            print('\t\t...sensor test successful.')
+            print('\t\t...sensor test successful.\n')
         elif result[1] == '0x1b' and result[2] == '0x41':
-            print('\t\t...sensor test failed. (WARNING, reader may still work)')
+            print('\t\t...sensor test failed. (WARNING, reader may still work)\n')
         else:
             sys.exit('Obtained unreadable result while testing the sensor.')
 
-    """ Test card reader ram. """
+    """ Test card reader memory. """
     def test_ram(self):
-        print('Testing card reader ram...')
+        print('Testing card reader memory...')
         msg = '\xc2%s' % TEST_RAM
         assert self.dev.ctrl_transfer(0x21, 9, 0x300, 0, msg) == len(msg)
-        ret = self.dev.read(0x81, 1024, 5000)
+
+        try:
+            ret = self.dev.read(0x81, 1024, 5000)
+        except usb.core.USBError as e:
+            self.reset()
+            print('\t\t...memory test timed out. (WARNING, reader may still work.\n')
+            return
 
         result = [hex(x) for x in ret]
         if result[1] == '0x1b' and result[2] == '0x30':
-            print('\t\t...ram test successful.')
+            print('\t\t...memory test successful.\n')
         elif result[1] == '0x1b' and result[2] == '0x41':
-            print('\t\t...ram test failed. (WARNING, reader may still work)')
+            print('\t\t...memory test failed. (WARNING, reader may still work)\n')
         else:
-            sys.exit('Obtained unreadable result while testing ram.')
+            sys.exit('Obtained unreadable result while testing memory.')
 
-
-    """ Read card with ISO format. """
-    def read_ISO(self):
-        self.reset()
-
-        msg = '\xc2%s' % READ_ISO
-        assert self.dev.ctrl_transfer(0x21, 9, 0x0300, 0, msg) == len(msg)
-
-        print('Waiting to process swipe...')
-
-        try:
-            ret = self.dev.read(0x81, 1024, 3000)
-            result = [hex(x).replace('0x', '') for x in ret]
-        except usb.core.USBError as e:
-            if str(e) == ('[Errno 110] Operation timed out'):
-                print('Reader timed out, please swipe again.')
-                return self.read_ISO()
-            else:
-                self.reset()
-                sys.exit('Read operation failed: %s' % str(e))
+    """ Parse ISO card data. """
+    def parse_ISO(self, data):
+        result = [hex(x).replace('0x', '') for x in data]
 
         # Start sentinal always begins on index 5, card data on 6 (track 1)
         fcode = codecs.decode(''.join(result[6:7]), 'hex')
@@ -131,20 +132,43 @@ class Reader:
         for v in self.card_data:
             print(v)
 
+    """ Read card with ISO format. """
+    def read_ISO(self):
+        self.reset()
+
+        msg = '\xc2%s' % READ_ISO
+        assert self.dev.ctrl_transfer(0x21, 9, 0x0300, 0, msg) == len(msg)
+
+        print('Waiting to process swipe...\n')
+
+        try:
+            data = self.dev.read(0x81, 1024, 3000)
+        except usb.core.USBError as e:
+            if str(e) == ('[Errno 110] Operation timed out'):
+                print('Reader timed out, please swipe again.')
+                return self.read_ISO()
+            else:
+                self.reset()
+                sys.exit('Read operation failed: %s' % str(e))
+
+        self.parse_ISO(data)
+
+        
     """ Initialize the device and claim it."""
     def claim_reader(self):
+        print('Initializing MagWorks...')
         self.dev = usb.core.find(idVendor = self.vid, idProduct = self.pid)
 
         # ensure msr exists
         if self.dev is None:
             sys.exit('MagWorks could not find any card readers.')
-        print('Card reader located.')
+        print('\t\t...located card reader.')
 
         # disable any existing drivers
         if self.dev.is_kernel_driver_active(0):
             try:
                 self.dev.detach_kernel_driver(0)
-                print('Existing kernel driver has been detached.')
+                print('\t\t...detached existing kernel driver.')
             except usb.core.USBError as e:
                 sys.exit('MagWorks was unable to detach the existing kernel driver: %s' % str(e))
 
@@ -154,8 +178,8 @@ class Reader:
             self.dev.reset()
         except usb.core.USBError as e:
             sys.exit('MagWorks failed to set the MSR configuration: %s' % str(e))
-        print('Card reader configuration set.')
+        print('\t\t...set card reader configuration.\n')
 
         self.test_comms()
-        self.test_sensor()
+        #self.test_sensor()
         self.test_ram()

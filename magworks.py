a0bc205c17aeb976f4785dc53ba9cb14c0c7aaaa
W. Wesley Weidenhamer II <www.wesley2@gmail.com>
2018-03-10 01:41:05 -0500

    add clone cmd, add text logo

--- magworks.py
+++ magworks.py
@@ -3,11 +3,20 @@ from reader import *
 
 version = '0.0.1'
 
+def logo():
+    print('______  ___              ___       __            ______         \n\
+___   |/  /_____ _______ __ |     / /_______________  /_________\n\
+__  /|_/ /_  __ `/_  __ `/_ | /| / /_  __ \_  ___/_  //_/_  ___/\n\
+_  /  / / / /_/ /_  /_/ /__ |/ |/ / / /_/ /  /   _  ,<  _(__  ) \n\
+/_/  /_/  \__,_/ _\__, / ____/|__/  \____//_/    /_/|_| /____/  \n\
+                 /____/                                         \n')
+
 def cli():
     parser = argparse.ArgumentParser(prog='magworks', description='A command-line tool for intefacing MSR devices.(MSR206/MSR605/MSR605X/MSR606). Operational arguments (such as -r, read) must be used individually, and will be processed in the order they appear below.', epilog='Developed by W. Wesley Weidenhamer II.')
     
     parser.add_argument('-r', choices=['iso', 'raw'], help='read data from a magnetic stripe card')
     parser.add_argument('-w', choices=['iso', 'raw'], help='write data to a magnetic stripe card')
+    parser.add_argument('-c', action='store_true', help='clone data from an existing card to a new one')
     parser.add_argument('-e', action='store_true', help='erase data from a magnetic stripe card')
     parser.add_argument('-t', choices=['conn', 'sensor', 'ram', 'led'], help='test connection and msr device')
     parser.add_argument('-m', action='store_true', help='get model of msr device')
@@ -15,6 +24,7 @@ def cli():
     parser.add_argument('-v', action='version', version='%(prog)s v' + version, help='display %(prog)s version')
 
     if len(sys.argv) < 2:
+        logo()
         parser.print_help()
         sys.exit(1)
     
@@ -31,6 +41,9 @@ def main():
         msr.read_ISO(0) if (args.r == 'iso') else msr.read_raw(0)
     
     elif args.w is not None:
+        print('This operation does not exist yet!')
+
+    elif args.c is not False:
         print('This operation does not exist yet!')
 
     elif args.e is not False:
